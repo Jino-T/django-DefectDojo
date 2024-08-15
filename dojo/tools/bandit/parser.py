8bcc0de96e15a067add2b60082468c1bcd4ae0a2
Jino Tesauro <jinotesauro@gmail.com>
2024-08-15 15:57:42 -0500

    Finished get_fields and get_dedupe_fields for Bandit Scanner

--- dojo/tools/bandit/parser.py
+++ dojo/tools/bandit/parser.py
@@ -74,3 +74,45 @@ class BanditParser(object):
             return 6
         else:
             return None
+
+def get_fields(self) -> list[str]:
+    """Return the list of fields used in the Bandit Parser.
+
+    Fields:
+    - title: Set to the issue_text outputted by th Bandit Scanner.
+    - description: Custom description made from: test_name, test_id, filename, line_number, issue_confidence, and code segments.
+    - severity: Set to issue_severity from Bandit Scanner.
+    - file_path: Set to filename from Bandit Scanner.
+    - line: Set to line from Bandit Scanner.
+    - date: Set to date from Bandit Scanner.
+    - vuln_id_from_tool: Made from joining test_name and test_id.
+    - nb_occurences: Initially set to 1 then updated.
+    - scanner_confidence: Based off of issue_confidence from Bandit Scanner.
+    """
+    return [
+        "title",
+        "test",
+        "description",
+        "severity",
+        "file_path",
+        "line",
+        "date",
+        "static_finding",
+        "dynamic_finding",
+        "vuln_id_from_tool",
+        "nb_occurences",
+    ]
+
+def get_dedupe_fields(self) -> list[str]:
+    """Return the list of fields used for deduplication in the Bandit Parser.
+
+    Fields:
+    - file_path: Set to filename from Bandit Scanner.
+    - line: Set to line from Bandit Scanner.
+    - vuln_id_from_tool: Made from joining test_name and test_id.
+    """
+    return [
+        "file_path",
+        "line",
+        "vuln_id_from_tool",
+    ]
